77d97814901db52ae226ff7d7ead9064118ab048
Shreya Ravikumar <v-sravikumar@microsoft.com>
2023-01-23 18:14:33 +0000

    Deleting unecessary files
    Signed-off-by: Raphi Kang <h.raphaela.kang@gmail.com>

--- vision-explanation-methods/__init__.py
+++ vision-explanation-methods/__init__.py
@@ -1,4 +1,4 @@
-"""Module for explaining vision models.
+"""Module for creating explanations for vision models.
 """
-__name__ = 'vision-explanation-methods'
+__name__ = "vision-explanation-methods"
 __version__ = '0.0.1'
