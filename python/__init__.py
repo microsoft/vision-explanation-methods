cb9a4ed732be2ef6b67ec06b37c86d89df0c709a
Ilya Matiach <ilmat@microsoft.com>
2023-01-26 15:19:09 -0500

    add setup.py file and version file, move files under python dir (#3)

---
+++ python/__init__.py
@@ -0,0 +1,10 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+"""Module for creating explanations for vision models.
+"""
+from .version import name, version
+
+__name__ = name
+__version__ = version
