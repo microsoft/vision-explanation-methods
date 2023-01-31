79b7a0293f18a67439f2b6b365ab2e835cfe0ac3
sravikumar19 <50054335+sravikumar19@users.noreply.github.com>
2023-01-31 16:21:58 -0500

    Fixing lint for DRISE_runner.py

--- python/vision_explanation_methods/DRISE_runner.py
+++ python/vision_explanation_methods/DRISE_runner.py
@@ -1,11 +1,12 @@
-import argparse
+"""Method for generating saliency maps for object detection models."""
+
 import os
 from typing import List, Optional, Tuple
 
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy
-import PIL.Image as Image
+from PIL import Image
 import torch
 import torchvision
 import torchvision.models.detection as detection
@@ -17,26 +18,29 @@ from .explanations import common as od_common
 from .explanations import drise
 
 
-class PytorchFasterRCNNWrapper(od_common.GeneralObjectDetectionModelWrapper):
-    """Wraps a PytorchFasterRCNN model with a predict API function for object detection.
+class PytorchFasterRCNNWrapper(
+        od_common.GeneralObjectDetectionModelWrapper):
+    """Wraps a PytorchFasterRCNN model with a predict API function.
 
-    To be compatible with the drise explainability method, all models must be wrapped to have
-    the same output and input class.
-    This wrapper is customized for the FasterRCNN model from Pytorch, and can
-    also be used with the RetinaNet or any other models with the same output class.
+    To be compatible with the D-RISE explainability method,
+    all models must be wrapped to have the same output and input class and a
+    predict function for object detection. This wrapper is customized for the
+    FasterRCNN model from Pytorch, and can also be used with the RetinaNet or
+    any other models with the same output class.
 
     :param model: Object detection model
     :type model: PytorchFasterRCNN model
     :param number_of_classes: Number of classes the model is predicting
     :type number_of_classes: int
     """
-    
+
     def __init__(self, model, number_of_classes: int):
+        """Initialize the PytorchFasterRCNNWrapper."""
         self._model = model
         self._number_of_classes = number_of_classes
 
     def predict(self, x: torch.Tensor) -> List[od_common.DetectionRecord]:
-        """Creates a list of detection records from the image predictions.
+        """Create a list of detection records from the image predictions.
 
         :param x: Tensor of the image
         :type x: torch.Tensor
@@ -45,32 +49,33 @@ class PytorchFasterRCNNWrapper(od_common.GeneralObjectDetectionModelWrapper):
         """
         raw_detections = self._model(x)
 
-        def apply_nms(orig_prediction: dict, iou_thresh: float=0.5):
-            """Performs non maximum suppression on the predictions according to their intersection-over-union.
+        def apply_nms(orig_prediction: dict, iou_thresh: float = 0.5):
+            """Perform nms on the predictions based on their IoU.
 
-            :param orig_prediction: Original model prediction 
+            :param orig_prediction: Original model prediction
             :type orig_prediction: dict
             :param iou_thresh: iou_threshold for nms
             :type iou_thresh: float
             :return: Model prediction after nms is applied
             :rtype: dict
             """
-            keep = torchvision.ops.nms(orig_prediction['boxes'], orig_prediction['scores'], iou_thresh)
+            keep = torchvision.ops.nms(orig_prediction['boxes'],
+                                       orig_prediction['scores'], iou_thresh)
 
             nms_prediction = orig_prediction
             nms_prediction['boxes'] = nms_prediction['boxes'][keep]
             nms_prediction['scores'] = nms_prediction['scores'][keep]
             nms_prediction['labels'] = nms_prediction['labels'][keep]
             return nms_prediction
-        
-        def filter_score(orig_prediction: dict, score_thresh: float=0.5):
-            """Filters out model predictions with confidence scores below score_thresh
 
-            :param orig_prediction: Original model prediction 
+        def filter_score(orig_prediction: dict, score_thresh: float = 0.5):
+            """Filter out predictions with confidence scores < score_thresh.
+
+            :param orig_prediction: Original model prediction
             :type orig_prediction: dict
             :param score_thresh: Score threshold to filter by
             :type score_thresh: float
-            :return: Model predictions filtered out by score_thresh 
+            :return: Model predictions filtered out by score_thresh
             :rtype: dict
             """
             keep = orig_prediction['scores'] > score_thresh
@@ -80,51 +85,58 @@ class PytorchFasterRCNNWrapper(od_common.GeneralObjectDetectionModelWrapper):
             filter_prediction['scores'] = filter_prediction['scores'][keep]
             filter_prediction['labels'] = filter_prediction['labels'][keep]
             return filter_prediction
-        
-        detections = [] 
+
+        detections = []
         for raw_detection in raw_detections:
-            raw_detection = apply_nms(raw_detection,0.005)
-            
-            # Note that FasterRCNN doesn't return a socre for each class, only the predicted class
-            # DRISE requires a score for each class. We approximate the score for each class
-            # by dividing the (1.0 - class score) evenly among the other classes.
-            
+            raw_detection = apply_nms(raw_detection, 0.005)
+
+            # Note that FasterRCNN doesn't return a socre for each class, only
+            # the predicted class. DRISE requires a score for each class.
+            # We approximate the score for each class
+            # by dividing (class score) evenly among the other classes.
+
             raw_detection = filter_score(raw_detection, 0.2)
-            expanded_class_scores = od_common.expand_class_scores(raw_detection['scores'],
-                                                                  raw_detection['labels'],
-                                                                  self._number_of_classes)
+            expanded_class_scores = od_common.expand_class_scores(
+                raw_detection['scores'],
+                raw_detection['labels'],
+                self._number_of_classes)
+
             detections.append(
                 od_common.DetectionRecord(
                     bounding_boxes=raw_detection['boxes'],
                     class_scores=expanded_class_scores,
-                    objectness_scores=torch.tensor([1.0]*raw_detection['boxes'].shape[0]),
-                    
+                    objectness_scores=torch.tensor(
+                        [1.0]*raw_detection['boxes'].shape[0]),
                 )
             )
-        
+
         return detections
 
-def plot_img_bbox(ax : matplotlib.axes._subplots, box: numpy.ndarray, label: str, color: str):
-    """Plots predicted bounding box and label on top of the d-rise generated saliency map.
 
-    :param ax: Axis on which the d-rise saliency map was plotted 
-    :type ax: Matplotlib Axes Subplot
+def plot_img_bbox(ax: matplotlib.axes._subplots, box: numpy.ndarray,
+                  label: str, color: str):
+    """Plot predicted bounding box and label on the D-RISE saliency map.
+
+    :param ax: Axis on which the d-rise saliency map was plotted
+    :type ax: Matplotlib AxesSubplot
     :param box: Bounding box the model predicted
     :type box: numpy.ndarray
-    :param label: Label the model predicted 
+    :param label: Label the model predicted
     :type label: str
     :param color: Color of the bounding box based on predicted label
     :type color: single letter color string
-    :return: Axis with the predicted bounding box and label plotted on top of d-rise saliency map
+    :return: Axis with the predicted bounding box and label plotted on top of
+        d-rise saliency map
     :rtype: Matplotlib AxesSubplot
     """
     x, y, width, height = box[0], box[1], box[2]-box[0], box[3]-box[1]
     rect = matplotlib.patches.Rectangle((x, y),
-                            width, height,
-                            linewidth = 2,
-                            edgecolor = color,
-                            facecolor = 'none',
-                            label=label)
+                                        width,
+                                        height,
+                                        linewidth=2,
+                                        edgecolor=color,
+                                        facecolor='none',
+                                        label=label)
     ax.add_patch(rect)
     frame = ax.get_position()
     ax.set_position([frame.x0, frame.y0, frame.width * 0.8, frame.height])
@@ -138,40 +150,43 @@ def get_instance_segmentation_model(num_classes: int):
 
     :param num_classes: Number of classes model predicted
     :type num_classes: int
-    :return: Faster R-CNN PyTorch model 
+    :return: Faster R-CNN PyTorch model
     :rtype: PyTorch model
     """
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(
+        pretrained=True)
     in_features = model.roi_heads.box_predictor.cls_score.in_features
-    # replace the pre-trained head with a new one
+    # Replace the pre-trained head with a new one
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-    
+
     return model
 
 
 def get_drise_saliency_map(
-    imagelocation: str,
-    model: Optional[object],
-    modellocation: Optional[str],
-    numclasses: int,
-    savename: str,
-    nummasks: int=25,
-    maskres: Tuple[int, int]=(4,4),
-    maskpadding: Optional[int]=None,
-    devicechoice: Optional[str]=None,
-    wrapperchoice: Optional[object] = PytorchFasterRCNNWrapper
-    ):
-    """Run D-RISE on image and visualize the saliency maps
+        imagelocation: str,
+        model: Optional[object],
+        modellocation: Optional[str],
+        numclasses: int,
+        savename: str,
+        nummasks: int = 25,
+        maskres: Tuple[int, int] = (4, 4),
+        maskpadding: Optional[int] = None,
+        devicechoice: Optional[str] = None,
+        wrapperchoice: Optional[object] = PytorchFasterRCNNWrapper
+):
+    """Run D-RISE on image and visualize the saliency maps.
 
-    :param imagelocation: Path for the image location.
+    :param imagelocation: Path of the image location
     :type imagelocation: str
-    :param model: Input model for D-RISE. If None, Faster R-CNN model will be used.
-    :type model: PyTorch model 
-    :param modellocation: Path for the model weights. If None, pre-trained Faster R-CNN model will be used.
+    :param model: Input model for D-RISE. If None, Faster R-CNN model
+        will be used.
+    :type model: PyTorch model
+    :param modellocation: Path of the model weights. If None, pre-trained
+        Faster R-CNN model will be used.
     :type modellocation: Optional str
     :param numclasses: Number of classes model predicted
     :type numclasses: int
-    :param savename: Path for the saved output figure. 
+    :param savename: Path of the saved output figure
     :type savename: str
     :param nummasks: Number of masks to use for saliency
     :type nummasks: int
@@ -183,87 +198,108 @@ def get_drise_saliency_map(
     :type devicechoice: str
     :param wrapperchoice: Choice to use fastrcnn wrapper or custom wrapper
     :type wrapperchoice: class
-    :return: Tuple of Matplotlib figure and string path to where the output figure is saved 
+    :return: Tuple of Matplotlib figure, path to where the output
+        figure is saved
     :rtype: Tuple of Matplotlib figure, str
     """
-    
     if not devicechoice:
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    else: device = devicechoice
+    else:
+        device = devicechoice
 
     if not model:
         if not modellocation:
-            # If user did not specify a model location, we simply load in the pytorch pre-trained model.
+            # If user did not specify a model location,
+            # we simply load in the pytorch pre-trained model.
             print("using pretrained fastercnn model")
-            model = detection.fasterrcnn_resnet50_fpn(pretrained=True,map_location=device)
+            model = detection.fasterrcnn_resnet50_fpn(pretrained=True,
+                                                      map_location=device)
             numclasses = 91
 
         else:
             print("loading user fastercnn model")
             model = get_instance_segmentation_model(numclasses)
-            model.load_state_dict(torch.load(modellocation,map_location=device))
+            model.load_state_dict(
+                torch.load(modellocation, map_location=device))
     else:
         if modellocation:
             print("loading any user model")
-            model.load_state_dict(torch.load(modellocation,map_location=device))
-
+            model.load_state_dict(
+                torch.load(modellocation, map_location=device))
 
     test_image = Image.open(imagelocation).convert('RGB')
 
     model = model.to(device)
     model.eval()
 
-    if not wrapperchoice: 
+    if not wrapperchoice:
         wrapperchoice = PytorchFasterRCNNWrapper
     explainable_wrapper = wrapperchoice(model, numclasses)
-    
 
-    detections = explainable_wrapper.predict(T.ToTensor()(test_image).unsqueeze(0).repeat(2, 1, 1, 1).to(device))
+    detections = explainable_wrapper.predict(
+        T.ToTensor()(test_image).unsqueeze(0).repeat(2, 1, 1, 1).to(device))
+
     saliency_scores = drise.DRISE_saliency(
         model=explainable_wrapper,
-        image_tensor=T.ToTensor()(test_image).repeat(2, 1, 1, 1).to(device), # Repeated the tensor to test batching.
+        # Repeated the tensor to test batching
+        image_tensor=T.ToTensor()(test_image).repeat(2, 1, 1, 1).to(device),
         target_detections=detections,
-        number_of_masks=nummasks, # This is how many masks to run - more is slower but gives higher quality mask.
+        # This is how many masks to run -
+        # more is slower but gives higher quality mask.
+        number_of_masks=nummasks,
         mask_padding=maskpadding,
         device=device,
-        mask_res=maskres, # This is the resolution of the random masks. High resolutions will give finer masks, but more need to be run.
-        verbose=True # Turns progress bar on/off.
-    ) 
+        # This is the resolution of the random masks.
+        # High resolutions will give finer masks, but more need to be run.
+        mask_res=maskres,
+        verbose=True  # Turns progress bar on/off.
+    )
 
     img_index = 0
-    saliency_scores = [saliency_scores[img_index][i] for i in range(len(saliency_scores[img_index])) 
-    if torch.isnan(saliency_scores[img_index][i]['detection']).any() == False] #exclude scores containing NaN
+
+    # Filter out saliency scores containing nan values
+    saliency_scores = [saliency_scores[img_index][i]
+                       for i in range(len(saliency_scores[img_index]))
+                       if not torch.isnan(
+                       saliency_scores[img_index][i]['detection']).any()]
+
     num_detections = len(saliency_scores)
 
-    if num_detections == 0: #if no objects have been detected... 
-        fail = Image.open(os.path.join("images","notfound.png")) 
+    if num_detections == 0:  # If no objects have been detected...
+        fail = Image.open(os.path.join("python", "vision_explanation_methods",
+                                       "images", "notfound.jpg"))
         fail = fail.save(savename)
-        return None,None
+        return None, None
 
-    fig, axis = plt.subplots(1, num_detections,figsize= (num_detections*10,10))
+    fig, axis = plt.subplots(1, num_detections,
+                             figsize=(num_detections*10, 10))
 
     for i in range(num_detections):
         viz.visualize_image_attr(
-            numpy.transpose(saliency_scores[i]['detection'].cpu().detach().numpy(), (1, 2, 0)), #drise runner usually has access to an images folder
-            # The [0][0] means first image, first detection.
+            numpy.transpose(
+                saliency_scores[i]['detection'].cpu().detach().numpy(),
+                (1, 2, 0)),
             numpy.transpose(T.ToTensor()(test_image).numpy(), (1, 2, 0)),
             method="blended_heat_map",
             sign="positive",
             show_colorbar=True,
             cmap=plt.cm.inferno,
             title="Detection " + str(i),
-            plt_fig_axis = (fig, axis[i]),
-            use_pyplot = False
+            plt_fig_axis=(fig, axis[i]),
+            use_pyplot=False
         )
 
-        box = detections[img_index].bounding_boxes[i].detach().numpy() 
-        label = int(torch.argmax(detections[img_index].class_scores[i]))  
-        if num_detections>1: #if there is more than one element to display, hence multiple subplots
-            #applicable only to recycling dataset.
-            axis[i] = plot_img_bbox(axis[i],box,str(label),'r')
-        elif type(axis)!=list: 
-            axis = plot_img_bbox(axis,box,str(label),'r')
-        else: #unclear why, but sometimes even with just one element axis needs to be indexed
-            axis[i] = plot_img_bbox(axis[i],box,str(label),'r')
+        box = detections[img_index].bounding_boxes[i].detach().numpy()
+        label = int(torch.argmax(detections[img_index].class_scores[i]))
+        # There is more than one element to display, hence multiple subplots
+        if num_detections > 1:
+            axis[i] = plot_img_bbox(axis[i], box, str(label), 'r')
+        elif type(axis) != list:
+            axis = plot_img_bbox(axis, box, str(label), 'r')
+        # Unclear why, but sometimes even with just one element,
+        # axis needs to be indexed
+        else:
+            axis[i] = plot_img_bbox(axis[i], box, str(label), 'r')
         fig.savefig(savename)
-    return fig,savename
+
+    return fig, savename
