2a9d3a625144ede5b4f7afdd0839084630441f32
Natalie Isak <natalieisak@microsoft.com>
2023-03-15 23:04:45 -0400

    drise runner

--- python/vision_explanation_methods/DRISE_runner.py
+++ python/vision_explanation_methods/DRISE_runner.py
@@ -296,7 +296,7 @@ def get_drise_saliency_map(
             method="blended_heat_map",
             sign="positive",
             show_colorbar=True,
-            cmap=plt.cm.gist_rainbow,
+            cmap=plt.cm.inferno,
             title="Detection " + str(i),
             plt_fig_axis=(fig, ax),
             use_pyplot=False
