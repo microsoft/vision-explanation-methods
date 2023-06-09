4729ac42c1df8545cd53c50f20d19c9c8a2629eb
natalie-isak <123979655+natalie-isak@users.noreply.github.com>
2023-06-09 14:00:32 -0400

    Creating Error List  (#39)
    * prototype of od error labeling
    
    * not working yet but almost
    
    * succeeded unit tests
    
    * linting
    
    * imports
    
    * cleanup
    
    * cleanup
    
    * imports
    
    * rename both
    
    * additional docs
    
    * rm prints
    
    * bg error
    
    * adding missing
    
    * rem print
    
    * in progress changes
    
    * label retrieval
    
    * example notebook
    
    * removing notebook to add to another PR
    
    * linting
    
    * error labeling linting
    
    * isort
    
    * error labeling
    
    * Update python/vision_explanation_methods/error_labeling/error_labeling.py
    
    Co-authored-by: Advitya Gemawat <agemawat@ucsd.edu>
    
    * Update python/vision_explanation_methods/error_labeling/error_labeling.py
    
    Co-authored-by: Advitya Gemawat <agemawat@ucsd.edu>
    
    * Update python/vision_explanation_methods/error_labeling/error_labeling.py
    
    Co-authored-by: Advitya Gemawat <agemawat@ucsd.edu>
    
    * Update python/vision_explanation_methods/error_labeling/error_labeling.py
    
    Co-authored-by: Advitya Gemawat <agemawat@ucsd.edu>
    
    * Update python/vision_explanation_methods/error_labeling/error_labeling.py
    
    Co-authored-by: Advitya Gemawat <agemawat@ucsd.edu>
    
    * Update python/vision_explanation_methods/error_labeling/error_labeling.py
    
    Co-authored-by: Advitya Gemawat <agemawat@ucsd.edu>
    
    * Update python/vision_explanation_methods/error_labeling/error_labeling.py
    
    Co-authored-by: Advitya Gemawat <agemawat@ucsd.edu>
    
    * Error labeling comments on pr
    
    * error labeling
    
    * error labeling linting
    
    * vem updates and comments
    
    * aligning indent
    
    * fixing unit test
    
    * tests
    
    * error labeling
    
    * unit tests
    
    * unit tests CONT
    
    * error labels
    
    ---------
    
    Co-authored-by: Advitya Gemawat <agemawat@ucsd.edu>

--- python/vision_explanation_methods/error_labeling/error_labeling.py
+++ python/vision_explanation_methods/error_labeling/error_labeling.py
@@ -3,6 +3,7 @@
 
 """Defines the Error Labeling Manager class."""
 
+from copy import deepcopy
 from enum import Enum
 
 import numpy as np
@@ -27,7 +28,8 @@ class ErrorLabelType(Enum):
     # this prediction must have a 0 iou score with all gt detections
     BACKGROUND = "background"
 
-    # the predicted class is correct, bounding box is not
+    # the predicted class is correct, bounding box does not have sufficient
+    # overlap with ground truth (based on the iou threshold)
     LOCALIZATION = "localization"
 
     # the predicted class is incorrect, the bounding box is correct
@@ -44,46 +46,52 @@ class ErrorLabelType(Enum):
 
 
 class ErrorLabeling():
-    """Defines a wrapper class of Error Labeling for vision scenario.
+    """
+    Defines a wrapper class of Error Labeling for vision scenario.
 
     Only supported for object detection at this point.
     """
 
     def __init__(self,
-                 pred_y: str,
-                 true_y: str,
+                 task_type: str,
+                 pred_y: list,
+                 true_y: list,
                  iou_threshold: float = 0.5):
-        """Create an ErrorLabeling object.
+        """
+        Create an ErrorLabeling object.
 
-        :param model: The model to explain.
-            A model that implements sklearn.predict or sklearn.predict_proba
-            or function that accepts a 2d ndarray.
-        :type model: object
-        :param evaluation_examples: A matrix of feature vector
-            examples (# examples x # features) on which to explain the
-            model's output, with an additional label column.
-        :type evaluation_examples: pandas.DataFrame
-        :param target_column: The name of the label column.
-        :type target_column: str
+        :param task_type: The task to run.
+        :type task_type: str
+        :param pred_y: predicted detections, nested list of 6 floats (class,
+            bounding box, conf score). The bounding box will be located at
+            indexes 1-4.
+        :type pred_y: list
+        :param true_y: ground truth detections, nested list of 6 floats (class,
+            bounding box, is crowded). The bounding box will be located at
+            indexes 1-4.
+        :type true_y: list
+        :param iou_threshold: required minimum for bounding box overlap
+        :type iou_threshold: float
         """
         self._is_run = False
         self._is_added = False
+        self._task_type = task_type
         self._pred_y = pred_y
         self._true_y = true_y
         self._iou_threshold = iou_threshold
-        self._match_matrix = np.full((len(self._true_y), len(self._pred_y)),
-                                     None)
 
-    def compute(self, **kwargs):
-        """Compute the error analysis data.
+    def compute_error_labels(self):
+        """
+        Compute labels for errors in an object detection prediction.
 
         Note: if a row does not have a match, that means that there is a
         missing gt detection
 
-        :param kwargs: The keyword arguments to pass to the compute method.
-            Note that this method does not take any arguments currently.
-        :type kwargs: dict
+        :return: 2d matrix of error labels
+        :rtype: NDArray
         """
+        match_matrix = np.full((len(self._true_y), len(self._pred_y)),
+                               None)
         # save original ordering of predictions
         original_indices = [i for i, _ in sorted(enumerate(self._pred_y),
                                                  key=lambda x: x[1][-1],
@@ -93,51 +101,142 @@ class ErrorLabeling():
         # this is to stay consistent with NMS and MAP algorithms
         sorted_list = sorted(self._pred_y, key=lambda x: x[-1], reverse=True)
 
+        if len(self._true_y) == 0:
+            match_matrix = [[ErrorLabelType.BACKGROUND]
+                            for _ in range(len(self._pred_y))]
+            return match_matrix
+
         for gt_index, gt in enumerate(self._true_y):
             for detect_index, detect in enumerate(sorted_list):
                 iou_score = torchvision.ops.box_iou(
                     Tensor(detect[1:5]).unsqueeze(0).view(-1, 4),
                     Tensor(gt[1:5]).unsqueeze(0).view(-1, 4))
 
-                if iou_score.item() == 0.0:
+                if iou_score.item() == 0:
                     # if iou is 0, then prediction is detecting the background
-                    self._match_matrix[gt_index][detect_index] = (
+                    match_matrix[gt_index][detect_index] = (
                         ErrorLabelType.BACKGROUND)
-                    continue
-                if (self._iou_threshold <= iou_score):
+                elif self._iou_threshold <= iou_score:
                     # the detection and ground truth bb's are overlapping
                     if detect[0] != gt[0]:
-                        # the bb's line up, but labels do not
-                        self._match_matrix[gt_index][detect_index] = (
+                        # the bboxes line up, but labels do not
+                        match_matrix[gt_index][detect_index] = (
                             ErrorLabelType.CLASS_NAME)
-                        continue
                     elif (ErrorLabelType.MATCH in
-                          self._match_matrix[gt_index]):
-                        # class name and bb correct, but there is already a
+                          match_matrix[gt_index]):
+                        # class name and bbox correct, but there is already a
                         # match with a higher confidence score (this is why
                         # it was imporant to sort by descending confidence
                         # scores as the first step)
-                        self._match_matrix[gt_index][detect_index] = (
+                        match_matrix[gt_index][detect_index] = (
                             ErrorLabelType.DUPLICATE_DETECTION)
-                        continue
                     else:
-                        # this means bbs overlap, class names = (1st time)
-                        self._match_matrix[gt_index][detect_index] = (
+                        # this means bboxes overlap, class names = (1st time)
+                        match_matrix[gt_index][detect_index] = (
                             ErrorLabelType.MATCH)
-                        continue
                 else:
                     if detect[0] != gt[0]:
-                        # the bb's don't line up, and labels do not
-                        self._match_matrix[gt_index][detect_index] = (
+                        # the bboxes don't line up, and labels do not
+                        match_matrix[gt_index][detect_index] = (
                             ErrorLabelType.CLASS_LOCALIZATION)
-                        continue
                     else:
-                        # the bb's don't line up, but the labels are correct
-                        self._match_matrix[gt_index][detect_index] = (
+                        # the bboxes don't line up, but the labels are correct
+                        match_matrix[gt_index][detect_index] = (
                             ErrorLabelType.LOCALIZATION)
-                        continue
 
-            # resort the columns (so no longer ordered by descending conf
-            # scores)
-            self._match_matrix[gt_index] = [self._match_matrix[gt_index][i]
-                                            for i in original_indices]
+        # resort the columns (so no longer ordered by descending conf
+        # scores)
+        match_matrix[gt_index] = [match_matrix[gt_index][i]
+                                  for i in original_indices]
+        return match_matrix
+
+    def compute_error_list(self):
+        """
+        Determine a complete list of errors encountered during prediction.
+
+        Note that it is possible to have more errors than actual objects
+        in an image (because we account for missing detections and
+        duplicate detections).
+        """
+        match_matrix = self.compute_error_labels()
+        error_arr = self._remove_matches(deepcopy(match_matrix))
+        dup_count = np.count_nonzero(match_matrix ==
+                                     ErrorLabelType.DUPLICATE_DETECTION)
+        error_list = [ErrorLabelType.DUPLICATE_DETECTION
+                      for _ in range(dup_count)]
+
+        if len(error_arr) == 0:
+            return error_list
+
+        diff = len(error_arr) - len(error_arr[0])
+        if diff > 0:
+            for _ in range(diff):
+                error_list.append(ErrorLabelType.MISSING)
+
+        order_of_errors = [ErrorLabelType.CLASS_NAME,
+                           ErrorLabelType.LOCALIZATION,
+                           ErrorLabelType.CLASS_LOCALIZATION,
+                           ErrorLabelType.BACKGROUND]
+
+        for err in order_of_errors:
+            for gt_index, gt in enumerate(error_arr):
+                for detect_index, detect in enumerate(gt):
+                    if detect == err:
+                        error_list.append(err)
+                        error_arr = self._remove_rows_cols(error_arr,
+                                                           set([gt_index]),
+                                                           set([detect_index]))
+                        if len(error_arr) == 0:
+                            break
+
+        return error_list
+
+    def _remove_matches(self, arr: np.array):
+        """
+        Remove match rows and columns from a error labeling matrix.
+
+        :param arr: np 2d array
+        :type arr: np.array
+        :return: array with removed rows and columns
+        :rtype: np.array
+        """
+        rows_to_delete = set()
+        cols_to_delete = set()
+
+        for row, row_items in enumerate(arr):
+            for col, value in enumerate(row_items):
+                if value == ErrorLabelType.MATCH:
+                    rows_to_delete.add(row)
+                    cols_to_delete.add(col)
+
+        modified_array = self._remove_rows_cols(arr,
+                                                rows_to_delete,
+                                                cols_to_delete)
+
+        return modified_array
+
+    def _remove_rows_cols(self,
+                          arr: np.array,
+                          rows_to_delete: set,
+                          cols_to_delete: set):
+        """
+        Remove rows and columns from a given array.
+
+        :param arr: np 2d array
+        :type arr: np.array
+        :param rows_to_delete: unique set of indexes of rows to remove
+        :type rows_to_delete: set
+        :param cols_to_delete: unique set of indexes of cols to remove
+        :type cols_to_delete: set
+        :return: array with removed rows and columns
+        :rtype: np.array
+        """
+        # Delete rows
+        modified_array = [row for row_index, row in enumerate(arr)
+                          if row_index not in rows_to_delete]
+
+        # Delete columns
+        modified_array = [[value for col_index, value in enumerate(row)
+                           if col_index not in cols_to_delete]
+                          for row in modified_array]
+        return modified_array
--- tests/test_error_labeling.py
+++ tests/test_error_labeling.py
@@ -114,7 +114,7 @@ class TestErrorLabelingManager(object):
         ([],
          [[0, 1, 1, 20, 20, 0]],
          .5,
-         np.array([None])),
+         np.array([ErrorLabelType.MISSING])),
 
         # edge case for background error (bb's touch, but iou is 0)
         ([[44, 5, 5, 1, 1, 0]],
@@ -128,8 +128,131 @@ class TestErrorLabelingManager(object):
                                              iou_threshold,
                                              result):
         """Compare _match_matrix attribute to expected result."""
-        mng = ErrorLabeling(pred_y,
+        task_type = 'object_detection'
+        mng = ErrorLabeling(task_type,
+                            pred_y,
                             true_y,
                             iou_threshold)
-        mng.compute()
-        assert (mng._match_matrix == result).all()
+        err = mng.compute_error_labels()
+        assert (err == result).all()
+
+    @pytest.mark.parametrize(("pred_y", "true_y", "iou_threshold", "result"), [
+        # correct instance, prediction exactly the same
+        ([[44, 162, 65, 365, 660, 0]],
+         [[44, 162, 65, 365, 660, 0]],
+         .5,
+         []),
+
+        # correct instance, predictions exactly the same for multiple detects
+        ([[44, 5, 5, 7, 7, 0], [44, 1, 1, 2, 2, 0]],
+         [[44, 5, 5, 7, 7, 0], [44, 1, 1, 2, 2, 0]],
+         .5,
+         []),
+
+        # correct instance, predictions exactly the same for multiple detects
+        # order is mixed up
+        ([[44, 5, 5, 7, 7, 0], [44, 1, 1, 2, 2, 0]],
+         [[44, 1, 1, 2, 2, 0], [44, 5, 5, 7, 7, 0]],
+         .5,
+         []),
+
+        # missing detection
+        ([[44, 5, 5, 7, 7, 0]],
+         [[44, 1, 1, 2, 2, 0], [44, 5, 5, 7, 7, 0]],
+         .5,
+         [np.array([ErrorLabelType.MISSING])]),
+
+        # correct instance, prediction not = but w/in iou threshold
+        ([[44, 162, 65, 365, 660, 0]],
+         [[44, 162, 65, 365, 670, 0]],
+         .5,
+         []),
+
+        # class error with 100% overlap
+        ([[44, 162, 65, 365, 660, 0]],
+         [[1, 162, 65, 365, 660, 0]],
+         .5,
+         np.array([ErrorLabelType.CLASS_NAME])),
+
+        # class error with overlapping bb, but not completely
+        ([[44, 162, 65, 365, 660, 0]],
+         [[1, 170, 65, 365, 660, 0]],
+         .5,
+         np.array([ErrorLabelType.CLASS_NAME])),
+
+        # complete miss, minor overlap with original (less than iou)
+        ([[1, 162, 65, 1000, 1000, 0]],
+         [[2, 162, 65, 1000, 200, 0]],
+         .5,
+         np.array([ErrorLabelType.CLASS_LOCALIZATION])),
+
+        # duplicate detection,
+        ([[1, 162, 65, 365, 660, 0], [1, 162, 65, 365, 660, 0]],
+         [[1, 162, 65, 365, 660, 0]],
+         .5,
+         np.array([ErrorLabelType.DUPLICATE_DETECTION])),
+
+        # duplicate detection, detections not identical but w/in iou range.
+        # same conf score
+        ([[1, 162, 65, 365, 660, 0], [1, 162, 65, 365, 659, 0]],
+         [[1, 162, 65, 365, 660, 0]],
+         .5,
+         np.array([ErrorLabelType.DUPLICATE_DETECTION])),
+
+        # duplicate detection, detections not identical but w/in iou range.
+        # Detection with lower iou has higher conf score
+        ([[1, 162, 65, 365, 660, 0], [1, 162, 65, 365, 659, 50]],
+         [[1, 162, 65, 365, 660, 0]],
+         .5,
+         np.array([ErrorLabelType.DUPLICATE_DETECTION])),
+
+        # Same as previous, but in a different order to ensure that conf score
+        # prioritization is working correctly
+        ([[1, 162, 65, 365, 659, 50], [1, 162, 65, 365, 660, 0]],
+         [[1, 162, 65, 365, 660, 0]],
+         .5,
+         np.array([ErrorLabelType.DUPLICATE_DETECTION])),
+
+        # background error with same class
+        ([[1, 50, 50, 100, 100, 0]],
+         [[1, 350, 350, 100, 100, 0]],
+         .5,
+         np.array([ErrorLabelType.BACKGROUND])),
+
+        # background error with different class
+        ([[0, 50, 50, 100, 100, 0]],
+         [[1, 350, 350, 100, 100, 0]],
+         .5,
+         np.array([ErrorLabelType.BACKGROUND])),
+
+        # gt is empty
+        ([[0, 1, 1, 20, 20, 0]],
+         [],
+         .5,
+         np.array([ErrorLabelType.BACKGROUND])),
+
+        # pred is empty
+        ([],
+         [[0, 1, 1, 20, 20, 0]],
+         .5,
+         np.array([ErrorLabelType.MISSING])),
+
+        # edge case for background error (bb's touch, but iou is 0)
+        ([[44, 5, 5, 1, 1, 0]],
+         [[44, 6, 6, 1, 1, 0]],
+         .5,
+         np.array([ErrorLabelType.BACKGROUND])),
+    ])
+    def test_object_detection_err_list_labeling(self,
+                                                pred_y,
+                                                true_y,
+                                                iou_threshold,
+                                                result):
+        """Compare error list to expected result."""
+        task_type = 'object_detection'
+        mng = ErrorLabeling(task_type,
+                            pred_y,
+                            true_y,
+                            iou_threshold)
+        lst = mng.compute_error_list()
+        assert (lst == result)
